1ffb225ec7b1f1fcfb4b856c98744147c49a7eca
JuantMartinez17 <juantomasm0@gmail.com>
2024-09-20 19:59:32 -0300

    Removed unused imports and dependencies

--- app/src/main/java/com/example/clase20_09/MainActivity.kt
+++ app/src/main/java/com/example/clase20_09/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.clase20_09
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.clase20_09.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
